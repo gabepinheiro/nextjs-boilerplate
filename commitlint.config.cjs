49432a8625a3a4bc88d8dc367131886d415f1673
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-09-25 17:40:33 -0300

    ➖ Dep-rm(setup): remove unnecessary packages

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,10 +1,3 @@
-const OFF = 0
-const ERROR = 2
-const HEADER_MAX_LENGTH = 72
-
 module.exports = {
-  rules: {
-    'header-max-length': [ERROR, 'always', HEADER_MAX_LENGTH],
-    'scope-case': [OFF],
-  },
+  extends: ['@commitlint/config-conventional'],
 }
