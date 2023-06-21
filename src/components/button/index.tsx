7181b3f15224fcdc16c0a499d5f25df544fe4862
Gabriel Pinheiro <45916330+gabepinheiro@users.noreply.github.com>
2023-06-20 21:31:32 -0300

    chore: update Nextjs to version 13 and update dependencies

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,3 +1,3 @@
-export function Button() {
-  return <button>MyButton</button>
+export function Button(props: React.HTMLAttributes<HTMLButtonElement>) {
+  return <button {...props}>MyButton</button>
 }
